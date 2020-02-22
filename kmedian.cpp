d7d2c69f013123d947dfc62c63ed7e661a3de39d
vardhanjain2012 <45758099+vardhanjain2012@users.noreply.github.com>
2020-02-22 10:57:19 +0530

    distance in O(1) time and O(n) space

--- kmedian.cpp
+++ kmedian.cpp
@@ -2,8 +2,8 @@
 #include<iostream>
 using namespace std;
 
-#define N 100
-#define k1 100
+#define N 1050
+#define k1 1050
 
 
 int k;
@@ -15,52 +15,40 @@ int num_nodes;
 int num_edges;
 int num_leaves;
 int tree[N];
+int postTree[N];
 int post_order_tree[N];
+int invPostOrder[N];
 int leaves[N];
 int m[N]; 
 int m_dash[N];
 int node_weight[N];
-int edge_weight[N][N];
+unordered_map<int,int> edge_weight;
 
-int D[N][N];
+int D[N];
 int tu1[N];
 
 int wt_below[N];
 
+set<int> nodes;
 set<int> arr[N];
+set<int> postArr[N];
 int leaf_map[N];
 
 
-
-
-
-
-
-void distance(int root,int gl_root){
-	int temp_dist=0;int temp=root;D[temp][temp]=0;
-	while(temp!=gl_root){
-		temp_dist+=edge_weight[temp][tree[temp]];
-		D[root][tree[temp]]=temp_dist;
-		temp=tree[temp];
-	}
-	if(!arr[root].empty()){
-		for(auto itr = arr[root].begin();itr!=arr[root].end();itr++){
-			distance((*itr),gl_root);
-		}
-	}
-	return;
+int create_distance(int i){
+	D[i]=i==root?0:create_distance(tree[i])+edge_weight[(i*num_nodes)+tree[i]];
+	return D[i];
+}
+int distance(int u,int v){
+	return (D[u]-D[v]);
 }
 
 int weight_below(int root){
-	wt_below[root]=0;
-	if(!arr[root].empty()){
-		int temp=0;
-		for(auto itr = arr[root].begin();itr!=arr[root].end();itr++){
-			temp+=weight_below((*itr));
-			temp+=node_weight[(*itr)];
-		}
-		wt_below[root]=temp;
+	wt_below[root]=node_weight[root];int temp=0;
+	for(auto itr = arr[root].begin();itr!=arr[root].end();itr++){
+		temp+=weight_below((*itr));
 	}
+	wt_below[root]+=temp;
 	return wt_below[root];
 }
 
@@ -68,7 +56,7 @@ int compute_tu1(int root){
 	tu1[root]=0;int temp=0;
 	if(!arr[root].empty()){
 		for(auto itr = arr[root].begin();itr!=arr[root].end();itr++){
-			temp+=(wt_below[(*itr)]+node_weight[(*itr)])*edge_weight[(*itr)][root]+compute_tu1((*itr));
+			temp+=(wt_below[(*itr)])*edge_weight[((*itr)*num_nodes)+root]+compute_tu1((*itr));
 		}
 		tu1[root]=temp;
 	}
@@ -114,8 +102,16 @@ int create_mv(int root){
 	}
 	return m[root];
 }
+
+void create_invpostorder(){
+	for(int i=0;i<num_nodes;i++){
+		invPostOrder[post_order_tree[i]]=i;
+	}
+	return;
+}
+
 void create_postorder(int root, int &index){
-	for(auto itr = arr[root].begin();itr!=arr[root].end();itr++){
+	for(auto itr = postArr[root].begin();itr!=postArr[root].end();itr++){
 		create_postorder((*itr),index);
 	}
 	post_order_tree[index]=root;
@@ -124,124 +120,181 @@ void create_postorder(int root, int &index){
 }
 
 void initialize(){
-	cin>>num_nodes>>num_edges>>k;
 	num_leaves=0;
-	if(num_nodes>N||num_edges>N*N){
-		cerr<<"Tree too large!";
-		return;
-	}
-	for(int i=0;i<num_nodes;i++){
+	for(int i=0;i<N;i++){
 		tree[i]=-1;
 	}
-	for(int i=0;i<num_nodes;i++){
-		cin>>node_weight[i];
-	}
-	for(int i=0;i<num_nodes;i++){
+	for(int i=0;i<N;i++){
 		arr[i].clear();
 	}
+	num_nodes=0;
+	num_edges=0;
+	ifstream myFile;
+	string line;
 	int temp;
-	for(int i=0;i<num_nodes;i++){
-		edge_weight[i][i]=0;
+	int temp2;
+	string wst;
+	myFile.open("trace.tree");
+	while(getline(myFile,line)){
+		stringstream ss(line);
+		ss>>temp;
+		nodes.insert(temp);
+		ss>>wst;
+		while(ss>>temp2){
+			nodes.insert(temp2);
+			postArr[temp].insert(temp2);
+			postTree[temp2]=temp;
+			num_edges++;
+		}
 	}
-	for(int i=0;i<num_edges;i++){
-		cin>>temp>>tree[temp];
-		cin>>edge_weight[temp][tree[temp]];
-		arr[tree[temp]].insert(temp);
-	}
+	myFile.close();
+
 	for(int i=0;i<num_nodes;i++){
-		if(tree[i]==-1){
+		if(postTree[i]==-1){
 			root=i;
 			break;
 		}
 	}
 
-	cout<<"The tree is:\n";
-	for(int i=0;i<num_nodes;i++){
-		cout<<tree[i]<<" ";
-	}
-	cout<<endl;
-
 
 	int index=0;
+	num_nodes=nodes.size();
 	create_postorder(root,index);
+	create_invpostorder();
 
-	if(index==num_nodes){
-		cout<<"The post order traversal is: \n";
-		for(int i=0;i<num_nodes;i++){
-			cout<<post_order_tree[i]<<" ";
+	for(int i=0;i<num_nodes;i++){
+		for(auto itr=postArr[post_order_tree[i]].begin();itr!=postArr[post_order_tree[i]].end();itr++){
+			arr[i].insert(invPostOrder[(*itr)]);
 		}
-		cout<<endl;
-	}
-	else{
-		cout<<"Something wrong\n";
 	}
 
+	for(int i=0;i<num_nodes;i++){
+		tree[i]=invPostOrder[postTree[post_order_tree[i]]];
+	}
+
+
+	myFile.open("trace.cost");
+	while(getline(myFile,line)){
+		stringstream ss(line);
+		ss>>temp;
+		ss>>wst;
+		ss>>node_weight[invPostOrder[temp]];
+	}
+	myFile.close();
+
+	root=invPostOrder[root];
+
+	for(int i=0;i<num_nodes;i++){
+		for(auto itr=arr[i].begin();itr!=arr[i].end();itr++){
+			edge_weight[((*itr)*num_nodes)+i]=1;
+		}
+	}
+
+	cin>>k;
+
+
+	cout<<"The post tree is:\n";
+	// for(int i=0;i<num_nodes;i++){
+	// 	cout<<postTree[i]<<" ";
+	// }
+	// cout<<endl;
+	
+	cout<<"The tree is:\n";
+	// for(int i=0;i<num_nodes;i++){
+	// 	cout<<tree[i]<<" ";
+	// }
+	// cout<<endl;
+
+
+
+	// if(index==num_nodes){
+	// 	cout<<"The post order traversal is: \n";
+	// 	for(int i=0;i<num_nodes;i++){
+	// 		cout<<post_order_tree[i]<<" ";
+	// 	}
+	// 	cout<<endl;
+	// }
+	// else{
+	// 	cout<<"Something wrong\n";
+	// }
+
+	cout<<"The root in postOrdertree is : "<<root<<endl<<endl;
 	create_mv(root);
 	cout<<"The mv is: \n";
-	for(int i=0;i<num_nodes;i++){
-		cout<<m[i]<<" ";
-	}
-	cout<<endl;
+	// for(int i=0;i<num_nodes;i++){
+	// 	cout<<m[i]<<" ";
+	// }
+	// cout<<endl;
 
 	index=0;
 	create_leaves(root,num_leaves);
 
 	cout<<"The leaves are: \n";
+	// for(int i=0;i<num_leaves;i++){
+	// 	cout<<leaves[i]<<" ";
+	// }
+	// cout<<endl;
+
 	for(int i=0;i<num_leaves;i++){
-		cout<<leaves[i]<<" ";
+		create_distance(leaves[i]);
 	}
-	cout<<endl;
 
 	create_mvd(root);
 
 	cout<<"The mv_dash is: \n";
-	for(int i=0;i<num_nodes;i++){
-		cout<<m_dash[i]<<" ";
-	}
-	cout<<endl;
+	// for(int i=0;i<num_nodes;i++){
+	// 	cout<<m_dash[i]<<" ";
+	// }
+	// cout<<endl;
+
 
 	index= weight_below(root);
 	index = compute_tu1(root);
 
 	cout<<"The wt_below is: \n";
-	for(int i=0;i<num_nodes;i++){
-		cout<<wt_below[i]<<" ";
-	}
-	cout<<endl;
+	// for(int i=0;i<num_nodes;i++){
+	// 	cout<<wt_below[i]<<" ";
+	// }
+	// cout<<endl;
 
 
 	cout<<"The tu1 is: \n";
-	for(int i=0;i<num_nodes;i++){
-		cout<<tu1[i]<<" ";
-	}
-	cout<<endl;
+	// for(int i=0;i<num_nodes;i++){
+	// 	cout<<tu1[i]<<" ";
+	// }
+	// cout<<endl;
+
+
 
 
 	distance(root,root);
 	cout<<"The distance is: \n";
-	for(int j=0;j<num_nodes;j++){
-		for(int i=0;i<num_nodes;i++){
-			cout<<D[j][i]<<" ";
-		}
-		cout<<endl;
-	}
-	cout<<endl;
-
-
+	// for(int j=0;j<num_nodes;j++){
+	// 	for(int i=0;i<num_nodes;i++){
+	// 		cout<<D[j][i]<<" ";
+	// 	}
+	// 	cout<<endl;
+	// }
+	// cout<<endl;
 
 	return;
 }
 void phase_one(){
 	for(int u=0;u<num_nodes;u++){
-		int T_u[num_nodes]={0}; //t_u[v]0
-		int R_u[num_nodes]={0}; //R_u[v]1
-		int L_u[num_nodes][k+1]={0}; //L_u_v_t;
-		
+		int* T_u = new int[num_nodes]; //t_u[v]0
+		int* R_u= new int[num_nodes]; //R_u[v]1
+		int** L_u=new int*[num_nodes]; //L_u_v_t;
+		// T[v][1]
 
-		int tmp_arr[u-m[u]+2];
+		for(int i=0;i<num_nodes;i++){
+			L_u[i]=new int[k+1];
+		}
+		// cout<<123<<endl;
+
+		int* tmp_arr = new int[u-m[u]+2];
 		tmp_arr[0]=0;
 		for(int i=0;i<(u-m[u]+1);i++){
-			tmp_arr[i+1]=tmp_arr[i]+node_weight[i+m[u]]*D[i+m[u]][u];
+			tmp_arr[i+1]=tmp_arr[i]+node_weight[i+m[u]]*distance(i+m[u],u);
 		}
 		for(int i=m[u];i<=u;i++){
 			T_u[i]= tmp_arr[i-m[u]+1]  - tmp_arr[m[i]-m[u]];
@@ -257,11 +310,9 @@ void phase_one(){
 			T[i][1]=tu1[i];
 		}
 
-
 		// initialize();
+		delete[] tmp_arr;
 		
-		
-
 
 		for(int t=2;t<=k;t++){
 			for(int v=m[u]+1;v<u;v++){
@@ -295,6 +346,14 @@ void phase_one(){
 				}
 			}
 		}
+			
+			delete[] T_u;
+			delete[] R_u;
+			for(int i=0;i<num_nodes;i++){
+				delete[] L_u[i];
+			}
+			delete[] L_u;
+
 		
 	}
 	return;
@@ -305,16 +364,20 @@ set<int> proxies(int u, int t){
 	set<int> res;
 	res.insert(u);
 	if(t==1) return res;
-	int T_u[num_nodes]={0}; //t_u[v]0
-	int R_u[num_nodes]={0}; //R_u[v]1
-	int L_u[num_nodes][k+1]={0}; //L_u_v_t;
+	int* T_u = new int[num_nodes]; //t_u[v]0
+	int* R_u= new int[num_nodes]; //R_u[v]1
+	int** L_u=new int*[num_nodes]; //L_u_v_t;
 	// T[v][1]
 
+	for(int i=0;i<num_nodes;i++){
+		L_u[i]=new int[k+1];
+	}
+	// cout<<123<<endl;
 
-	int tmp_arr[u-m[u]+2];
+	int* tmp_arr = new int[u-m[u]+2];
 	tmp_arr[0]=0;
 	for(int i=0;i<(u-m[u]+1);i++){
-		tmp_arr[i+1]=tmp_arr[i]+node_weight[i+m[u]]*D[i+m[u]][u];
+		tmp_arr[i+1]=tmp_arr[i]+node_weight[i+m[u]]*distance(i+m[u],u);
 	}
 	for(int i=m[u];i<=u;i++){
 		T_u[i]= tmp_arr[i-m[u]+1]  - tmp_arr[m[i]-m[u]];
@@ -335,10 +398,14 @@ set<int> proxies(int u, int t){
 	
 
 
-	int N_u[k+1];
-	int C_u[k+1];
-	int x_u[num_nodes][k+1];
-	int c_u[num_nodes][k+1];
+	int* N_u = new int[k+1];
+	int* C_u = new int[k+1];
+	int** x_u=new int*[num_nodes];
+	int** c_u=new int*[num_nodes];
+	for(int i=0;i<num_nodes;i++){
+		x_u[i]=new int[k+1];
+		c_u[i]=new int[k+1];
+	}
 	for(int t=2;t<=k;t++){
 		for(int v=m[u]+1;v<u;v++){
 			if(v==m[v] && m[v]!=m[u]){
@@ -395,12 +462,15 @@ set<int> proxies(int u, int t){
 	delete[] T_u;
 	delete[] R_u;
 	for(int i=0;i<num_nodes;i++){
-		delete[] L_u;
-		delete[] x_u;
-		delete[] c_u;
+		delete[] L_u[i];
+		delete[] x_u[i];
+		delete[] c_u[i];
 
 	}
 
+	delete[] L_u;
+	delete[] x_u;
+	delete[] c_u;
 	for(int temp=1;temp<i;temp++){
 		set<int> temp_set = proxies(N_u[i-temp],C_u[i-temp]);
 		res.insert(temp_set.begin(),temp_set.end());
@@ -408,13 +478,30 @@ set<int> proxies(int u, int t){
 	return res;
 }
 int main(){
+
+	clock_t start_time,end_time;
+	start_time=clock();
 	initialize();
+	cout<<"Initialization done\n";
 	phase_one();
+	cout<<"Phase one completed\n";
+	for(int i=0;i<0;i++){
+
+	}
     set<int> result = proxies(root,k);
-    cout<<"The proxies are: \n";
+    cout<<"The proxies in postOrdertree are: \n";
     for(auto i:result){
     	cout<<i<<' ';
     }
     cout<<endl;
+
+    cout<<"The proxies in Actualtree are: \n";
+    for(auto i:result){
+    	cout<<post_order_tree[i]<<' ';
+    }
+    cout<<endl;
+
+    end_time=clock();
+	cout<<"\nTime taken: "<<(double)(end_time-start_time)<<" milliseconds"<<endl;
 	return 0;
 }
